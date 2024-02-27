537b71b57363b187b0a35261beaf025a73dc2e6f
Nicolo Castelnuovo <ncasteln@2-H-4.42heilbronn.de>
2024-02-27 17:21:34 +0100

    Trying new indentation

--- cpp_05/ex00/Bureaucrat.cpp
+++ cpp_05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: ncasteln <ncasteln@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 14:59:42 by ncasteln          #+#    #+#             */
-/*   Updated: 2024/02/27 12:03:44 by ncasteln         ###   ########.fr       */
+/*   Updated: 2024/02/27 17:21:13 by ncasteln         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,54 @@
 
 // ------------------------------------------------------------------ CANONICAL
 Bureaucrat::Bureaucrat( void ): _grade(150), _name("<bureaucrat no name>") {
-	std::cout << "[Bureaucrat] default constructor" << std::endl;
+  std::cout << "[Bureaucrat] default constructor" << std::endl;
 }
 
 Bureaucrat::~Bureaucrat( void ) {
-	// std::cout << "[Bureaucrat] destructor" << std::endl; // removed for readability
+  // std::cout << "[Bureaucrat] destructor" << std::endl; // removed for readability
 }
 
 Bureaucrat::Bureaucrat( const Bureaucrat& obj ):
-	_grade(obj._grade),
-	_name(obj._name) {
-	std::cout << "[Bureaucrat] copy constructor" << std::endl;
+  _grade(obj._grade),
+  _name(obj._name) {
+  std::cout << "[Bureaucrat] copy constructor" << std::endl;
 }
 
 // --------------------------------------------------------- EXTRA CONSTRUCTORS
 Bureaucrat::Bureaucrat( const std::string name, int grade ): _name(name) {
-	std::cout << "[Bureaucrat] [std::string, int] constructor" << std::endl;
-	std::cout << this->_name << " " << grade;
-	if (grade < 1)
-		throw GradeTooHighException(EXCEP_CREATE);
-	if (grade > 150)
-		throw GradeTooLowException(EXCEP_CREATE);
-	std::cout << ": created" << std::endl;
-	this->_grade = grade;
+  std::cout << "[Bureaucrat] [std::string, int] constructor" << std::endl;
+  std::cout << this->_name << " " << grade;
+  if (grade < 1)
+    throw GradeTooHighException(EXCEP_CREATE);
+  if (grade > 150)
+    throw GradeTooLowException(EXCEP_CREATE);
+  std::cout << ": created" << std::endl;
+  this->_grade = grade;
 }
 
 // ------------------------------------------------------------------ FUNCTIONS
 const std::string Bureaucrat::getName( void ) const {
-	return (this->_name);
+  return (this->_name);
 }
 
 int Bureaucrat::getGrade( void ) const {
-	return (this->_grade);
+  return (this->_grade);
 }
 
 void Bureaucrat::incrementGrade( void ) {
-	std::cout << this->_name;
-	if (this->_grade == 1)
-		throw GradeTooHighException(EXCEP_INCR);
-	std::cout << ": grade incremented" << std::endl;
-	this->_grade--;
+  std::cout << this->_name;
+  if (this->_grade == 1)
+    throw GradeTooHighException(EXCEP_INCR);
+  std::cout << ": grade incremented" << std::endl;
+  this->_grade--;
 }
 
 void Bureaucrat::decrementGrade( void ) {
-	std::cout << this->_name;
-	if (this->_grade == 150)
-		throw GradeTooLowException(EXCEP_DECR);
-	std::cout << ": grade decremented" << std::endl;
-	this->_grade++;
+  std::cout << this->_name;
+  if (this->_grade == 150)
+    throw GradeTooLowException(EXCEP_DECR);
+  std::cout << ": grade decremented" << std::endl;
+  this->_grade++;
 }
 
 // --------------------------------------------------------------------- NESTED
@@ -70,23 +70,23 @@ Bureaucrat::GradeTooLowException::GradeTooLowException( int excep ): _excep_n(ex
 Bureaucrat::GradeTooHighException::GradeTooHighException( int excep ): _excep_n(excep) {}
 
 const char* Bureaucrat::GradeTooLowException::what() const throw() {
-	if (this->_excep_n == EXCEP_DECR)
-		return (": cannot decrement");
-	if (this->_excep_n == EXCEP_CREATE)
-		return (": cannot create Bureaucrat with grade lower than 150");
-	return (": grade too low to be used");
+  if (this->_excep_n == EXCEP_DECR)
+    return (": cannot decrement");
+  if (this->_excep_n == EXCEP_CREATE)
+    return (": cannot create Bureaucrat with grade lower than 150");
+  return (": grade too low to be used");
 }
 
 const char* Bureaucrat::GradeTooHighException::what() const throw() {
-	if (this->_excep_n == EXCEP_INCR)
-		return (": cannot increment");
-	if (this->_excep_n == EXCEP_CREATE)
-		return (": cannot create Bureaucrat with negative grade");
-	return (": grade too high to be used");
+  if (this->_excep_n == EXCEP_INCR)
+    return (": cannot increment");
+  if (this->_excep_n == EXCEP_CREATE)
+    return (": cannot create Bureaucrat with negative grade");
+  return (": grade too high to be used");
 }
 
 // ------------------------------------------------------------------ OVERLOADS
 std::ostream& operator<<( std::ostream& cout, const Bureaucrat& b ) {
-	cout << b.getName() << ", bureaucrat grade " << b.getGrade() << ".";
-	return (cout);
+  cout << b.getName() << ", bureaucrat grade " << b.getGrade() << ".";
+  return (cout);
 }
--- cpp_05/ex00/Bureaucrat.hpp
+++ cpp_05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: ncasteln <ncasteln@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 14:55:58 by ncasteln          #+#    #+#             */
-/*   Updated: 2024/02/27 12:57:40 by ncasteln         ###   ########.fr       */
+/*   Updated: 2024/02/27 17:17:03 by ncasteln         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,57 +22,57 @@
 # define EXCEP_INCR		2
 
 /*
-	operator= is made not-usable because of const _name. In that way I can
-	void someone that assign a Bureaucrat to another one by accidental mistake.
+  operator= is made not-usable because of const _name. In that way I can
+  void someone that assign a Bureaucrat to another one by accidental mistake.
 */
 class Bureaucrat
 {
-	public:
-		// --------------------------------------------- CANONICAL CONSTRUCTORS
-		Bureaucrat( void );
-		virtual ~Bureaucrat( void );
-		Bureaucrat( const Bureaucrat& );
+  public:
+    // --------------------------------------------- CANONICAL CONSTRUCTORS
+    Bureaucrat( void );
+    virtual ~Bureaucrat( void );
+    Bureaucrat( const Bureaucrat& );
 
-		// ------------------------------------------------- OTHER CONSTRUCTORS
-		Bureaucrat( const std::string, int );
+    // ------------------------------------------------- OTHER CONSTRUCTORS
+    Bureaucrat( const std::string, int );
 
-		// ------------------------------------------------------------ GETTERS
-		const std::string getName( void ) const;
-		int getGrade( void ) const;
+    // ------------------------------------------------------------ GETTERS
+    const std::string getName( void ) const;
+    int getGrade( void ) const;
 
-		// ------------------------------------------------------------ SETTERS
-		void incrementGrade( void );
-		void decrementGrade( void );
+    // ------------------------------------------------------------ SETTERS
+    void incrementGrade( void );
+    void decrementGrade( void );
 
-		// -------------------------------------------------------------- EXCEP
-		class GradeTooLowException;
-		class GradeTooHighException;
-	private:
-		int _grade;
-		const std::string _name;
-		Bureaucrat& operator=( const Bureaucrat& );	// not-usable
+    // -------------------------------------------------------------- EXCEP
+    class GradeTooLowException;
+    class GradeTooHighException;
+  private:
+    int _grade;
+    const std::string _name;
+    Bureaucrat& operator=( const Bureaucrat& );	// not-usable
 };
 
 /*
-	The constructor is not requested by the subject, it is made just to create
-	a more detailed error message.
+  The constructor is not requested by the subject, it is made just to create
+  a more detailed error message.
 */
 class Bureaucrat::GradeTooLowException: public std::exception
 {
-	public:
-		GradeTooLowException( int );
-		const char* what() const throw();
-	private:
-		int _excep_n;
+  public:
+    GradeTooLowException( int );
+    const char* what() const throw();
+  private:
+    int _excep_n;
 };
 
 class Bureaucrat::GradeTooHighException: public std::exception
 {
-	public:
-		GradeTooHighException( int );
-		const char* what() const throw();
-	private:
-		int _excep_n;
+  public:
+    GradeTooHighException( int );
+    const char* what() const throw();
+  private:
+    int _excep_n;
 };
 
 std::ostream& operator<<( std::ostream&, const Bureaucrat& );
